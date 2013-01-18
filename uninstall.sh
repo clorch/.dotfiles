8fcc4e1dcdc3fe7ddb9318cd442cded150ead1e9
Clemens Horch <mail@clemenshorch.de>
2013-01-18 10:40:55 +0100

    initial commit

---
+++ uninstall.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#
+# Modeled after https://github.com/holman/dotfiles/blob/master/Rakefile
+# taken from https://raw.github.com/robb/.dotfiles/uninstall.sh
+
+symlinks=$(find . -name "*.symlink")
+
+for file in $symlinks; do
+    basename=$(basename $file .symlink)
+    target="$HOME/$basename"
+    backup="$HOME/$basename.backup"
+
+    if [ -h "$target" ]; then
+        rm $target
+    fi
+
+    if [ -e "$backup" ] || [ -h "$backup" ]; then
+        mv $backup $target
+    fi
+done
